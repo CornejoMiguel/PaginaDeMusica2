c999864fe9d99c7dd5ce0ae1ec4a51aeabbeb152
CornejoMiguel <mnaxy16@gmail.com>
2018-02-14 09:18:07 +0100

    Cambios

---
+++ enlaces/PaginaPrincipal.js
@@ -0,0 +1,8 @@
+var videocontainer = document.getElementById('videoclip');
+var videosource = document.getElementById('videosource');
+
+function MostrarVideo(boton) {
+    videocontainer.pause();
+    videosource.setAttribute('src', "VideosMusicales/" + boton.id + ".mp4");
+    videocontainer.load();
+};
